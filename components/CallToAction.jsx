79d6c2090c7ae08de694bc97fa6ebe1d2e360cf3
praharshaAdhikari <114891918+praharshaAdhikari@users.noreply.github.com>
2023-10-15 19:29:32 +0545

    Day 1

--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -3,8 +3,6 @@ const CallToAction = () => {
     <div className="callToAction-image rounded-xl h-96 md:my-24 my-12 mx-auto text-white text-center w-4/5">
         <h1 className="md:text-6xl text-4xl font-semibold sm:font-bold mt-32 mb-8 w-2/3 mx-auto">Enroll for a PROGRAM</h1>
         <a className="border uppercase font-semibold rounded-sm border-white py-4 px-8 mt-4 inline-block hover:bg-lime-500 hover:border-lime-500 duration-300" href="/">Contact Us</a>
-
-        {/* <a className="border uppercase font-semibold rounded-sm border-white py-4 px-8 inline-block hover:bg-lime-500 hover:border-lime-500 duration-300" href="/">Contact Us</a> */}
     </div>
   )
 }
